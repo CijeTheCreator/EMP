f9d7fd40a8cba60de1f6a414601af378912e3418
Chijioke <osadebec98@gmail.com>
2024-12-05 21:53:22 +0100

    Auto-commit: Thu Dec  5 09:53:22 PM WAT 2024

--- app/index.ts
+++ app/index.ts
@@ -1,25 +1,152 @@
-import express, { Express } from "express";
-import cors from "cors";
+import { Prisma, PrismaClient } from "@prisma/client";
+import express from "express";
 
-import dotenv from "dotenv";
-import path from "path";
-import AppConstants from "./utils/constants";
-import logger from "./utils/logger";
+const prisma = new PrismaClient();
+const app = express();
 
-const configPath = path.join(__dirname, "../config");
+app.use(express.json());
 
-const envFile =
-  process.env.NODE_ENV === AppConstants.Environments.production
-    ? ".env.production"
-    : ".env.development";
+app.post(`/signup`, async (req, res) => {
+  const { name, email, posts } = req.body;
 
-dotenv.config({ path: path.join(configPath, envFile) });
+  const postData = posts?.map((post: Prisma.PostCreateInput) => {
+    return { title: post?.title, content: post?.content };
+  });
 
-const PORT = process.env.PORT || 8000;
-
-const app: Express = express();
-app.use(cors());
-
-app.listen(PORT, () => {
-  logger.info(`[SERVER] Running at http://localhost:${PORT}`);
+  const result = await prisma.user.create({
+    data: {
+      name,
+      email,
+      posts: {
+        create: postData,
+      },
+    },
+  });
+  res.json(result);
 });
+
+app.post(`/post`, async (req, res) => {
+  const { title, content, authorEmail } = req.body;
+  const result = await prisma.post.create({
+    data: {
+      title,
+      content,
+      author: { connect: { email: authorEmail } },
+    },
+  });
+  res.json(result);
+});
+
+app.put("/post/:id/views", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const post = await prisma.post.update({
+      where: { id: Number(id) },
+      data: {
+        viewCount: {
+          increment: 1,
+        },
+      },
+    });
+
+    res.json(post);
+  } catch (error) {
+    res.json({ error: `Post with ID ${id} does not exist in the database` });
+  }
+});
+
+app.put("/publish/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const postData = await prisma.post.findUnique({
+      where: { id: Number(id) },
+      select: {
+        published: true,
+      },
+    });
+
+    const updatedPost = await prisma.post.update({
+      where: { id: Number(id) || undefined },
+      data: { published: !postData?.published },
+    });
+    res.json(updatedPost);
+  } catch (error) {
+    res.json({ error: `Post with ID ${id} does not exist in the database` });
+  }
+});
+
+app.delete(`/post/:id`, async (req, res) => {
+  const { id } = req.params;
+  const post = await prisma.post.delete({
+    where: {
+      id: Number(id),
+    },
+  });
+  res.json(post);
+});
+
+app.get("/users", async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
+});
+
+app.get("/user/:id/drafts", async (req, res) => {
+  const { id } = req.params;
+
+  const drafts = await prisma.user
+    .findUnique({
+      where: {
+        id: Number(id),
+      },
+    })
+    .posts({
+      where: { published: false },
+    });
+
+  res.json(drafts);
+});
+
+app.get(`/post/:id`, async (req, res) => {
+  const { id }: { id?: string } = req.params;
+
+  const post = await prisma.post.findUnique({
+    where: { id: Number(id) },
+  });
+  res.json(post);
+});
+
+app.get("/feed", async (req, res) => {
+  const { searchString, skip, take, orderBy } = req.query;
+
+  const or: Prisma.PostWhereInput = searchString
+    ? {
+        OR: [
+          { title: { contains: searchString as string } },
+          { content: { contains: searchString as string } },
+        ],
+      }
+    : {};
+
+  const posts = await prisma.post.findMany({
+    where: {
+      published: true,
+      ...or,
+    },
+    include: { author: true },
+    take: Number(take) || undefined,
+    skip: Number(skip) || undefined,
+    orderBy: {
+      updatedAt: orderBy as Prisma.SortOrder,
+    },
+  });
+
+  res.json(posts);
+});
+
+const server = app.listen(3000, () =>
+  console.log(`
+🚀 Server ready at: http://localhost:3000
+⭐️ See sample requests: http://pris.ly/e/ts/rest-express#3-using-the-rest-api`),
+);
